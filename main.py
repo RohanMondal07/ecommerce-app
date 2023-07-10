0d075707eead89e1d50879e4b2ca31bd856646a0
Rohan Mondal <117859167+RohanMondal07@users.noreply.github.com>
2023-07-10 16:49:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,84 @@
+from datetime import datetime
+from bson import ObjectId
+from fastapi import FastAPI, status, Query, HTTPException, Body
+import motor.motor_asyncio
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+from typing import List
+from pydantic import BaseModel
 import uvicorn
-from fastapi import FastAPI
-from motor import motor_asyncio
-from models import Product, Order
 
 app = FastAPI()
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://ecom123:pass1234@ecommerce.tdtbegi.mongodb.net/?retryWrites=true&w=majority")
+db = client.ecommerce
 
-# MongoDB connection
-client = motor_asyncio.AsyncIOMotorClient("mongodb+srv://ecom123:pass1234@ecommerce.tdtbegi.mongodb.net/")
-db = client["ecommerce"]
-products_collection = db["products"]
-orders_collection = db["orders"]
+class Product(BaseModel):
+    name: str
+    price: float
+    available_quantity: int
 
+class OrderItem(BaseModel):
+    product_id: str
+    bought_quantity: int
 
-# API endpoints
-@app.get("/products")
-async def list_products():
-    products = await products_collection.find().to_list(20)
+class Order(BaseModel):
+    timestamp: str
+    items: List[OrderItem]
+    total_amount: float
+    user_address: dict[str, str]
+
+@app.get("/", response_description="Home")
+async def home():
+    return {"status": "ok"}
+
+@app.get("/get-products/", response_description="List all products", response_model=List[Product])
+async def list_products(limit: int = Query(10, ge=1, le=100), offset: int = Query(0, ge=0)):
+    if limit < 1 or limit > 10:
+        raise HTTPException(status_code=400, detail="Invalid limit. Limit must be between 1 and 100.")
+    if offset < 0:
+        raise HTTPException(status_code=400, detail="Invalid offset. Offset must be greater than or equal to 0.")
+    products = await db["Product"].find().skip(offset).limit(limit).to_list(length=None)
     return products
 
-@app.post("/orders")
-async def create_order(order: Order):
-    order_id = await orders_collection.insert_one(order.dict()).inserted_id
-    return {"order_id": str(order_id)}
+@app.post("/post-products/", response_description="Add new product", response_model=Product)
+async def create_product(product: Product = Body(...)):
+    product = jsonable_encoder(product)
+    new_product = await db["products"].insert_one(product)
+    created_product = await db["products"].find_one({"_id": new_product.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_product)
 
-@app.get("/orders")
-async def fetch_orders(limit: int = 10, offset: int = 0):
-    orders = await orders_collection.find().skip(offset).limit(limit).to_list(limit)
+@app.put("/edit-products/{product_id}", response_description="Update product")
+async def update_product(product_id: str, quantity: int):
+    existing_product = await db["products"].find_one({"_id": product_id})
+    if not existing_product:
+        raise HTTPException(status_code=404, detail=f"Product with ID {product_id} not found.")
+    await db["products"].update_one({"_id": product_id}, {"$set": {"available_quantity": quantity}})
+    updated_product = await db["products"].find_one({"_id": product_id})
+    return JSONResponse(status_code=status.HTTP_200_OK, content=updated_product)
+
+@app.get("/get-orders/", response_description="List all orders", response_model=List[Order])
+async def list_orders(limit: int = Query(10, ge=1, le=10), offset: int = Query(0, ge=0)):
+    if limit < 1 or limit > 10:
+        raise HTTPException(status_code=400, detail="Invalid limit. Limit must be between 1 and 1000.")
+    if offset < 0:
+        raise HTTPException(status_code=400, detail="Invalid offset. Offset must be greater than or equal to 0.")
+    orders = await db["orders"].find().skip(offset).limit(limit).to_list(length=None)
     return orders
 
-@app.get("/orders/{order_id}")
-async def fetch_order(order_id: str):
-    order = await orders_collection.find_one({"_id": order_id})
-    if order:
-        return order
-    return {"message": "Order not found"}
+@app.get("/orders/{order_id}", response_description="Get a single order", response_model=Order)
+async def get_order(order_id: str):
+    order = await db["orders"].find_one({"_id": ObjectId(order_id)})
+    if not order:
+        raise HTTPException(status_code=404, detail=f"Order with ID {order_id} not found.")
+    return order
 
-@app.put("/products/{product_id}")
-async def update_product(product_id: str, quantity: int):
-    result = await products_collection.update_one(
-        {"_id": product_id},
-        {"$set": {"available_quantity": quantity}}
-    )
-    if result.modified_count:
-        return {"message": "Product updated successfully"}
-    return {"message": "Product not found"}
-
+@app.post("/post-orders/", response_description="Add new order")
+async def create_order(order: Order):
+    order_dict = order.dict()
+    order_dict["timestamp"] = datetime.now().isoformat()
+    new_order = await db["orders"].insert_one(order_dict)
+    created_order = await db["orders"].find_one({"_id": new_order.inserted_id})
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_order)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
